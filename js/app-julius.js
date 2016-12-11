6db7a9ce30511c3164315b3f1d656a057e6dd1d1
Julius Alvarado <angularjs.uiux@gmail.com>
2016-12-10 23:58:06 -0800

    added content
    made it a prettier ui

---
+++ js/app-julius.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Julius Alvarado on 12/10/2016.
+ */
+
+jQuery(function(){
+    jQuery(window).on('load resize', function(){
+        jQuery('.fill-screen').css('height', window.innerHeight);
+    });
+});
+
+//\\
