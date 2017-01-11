853a7aae076fdd61aaeb5c5577ab1432185b9118
Julius Alvarado <angularjs.uiux@gmail.com>
2017-01-11 11:17:50 -0800

    couple of quick fixes
    did some media queries, and figured out why there was an x overflow.

--- js/app-julius.js
+++ js/app-julius.js
@@ -32,6 +32,22 @@ jQuery(function () {
         }
     });
 
+    //jQuery(window).stellar();
+
+    // own carousel code
+    jQuery('.skill-carousel').owlCarousel({
+        singleItem: true,
+        navigation: true,
+        pagination: false,
+        navigationText: [
+            '<i class="fa fa-angle-left"></i>',
+            '<i class="fa fa-angle-right"></i>'
+        ],
+        autoHeight: true,
+        mouseDrag: false,
+        touchDrag: false,
+        transitionStyle: 'fadeUp'
+    });
 
     // Form Handling Code
 
